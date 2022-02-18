62bbf71bbc64405d9a418729fdbc405a875dc391
Emil <ionascu.emil@gmail.com>
2022-02-18 12:16:08 +0200

    Parse data write a helper function to parse our data

---
+++ src/Helpers/parse-data.js
@@ -0,0 +1,33 @@
+/* eslint-disable import/prefer-default-export */
+import { DateTime } from 'luxon';
+import { sortBy, prop } from 'ramda';
+
+export function parseData(data) {
+  const getYear = (timestamp) => (DateTime.fromMillis(Number(timestamp)).year);
+  const getMonth = (timestamp) => (DateTime.fromMillis(Number(timestamp)).monthShort);
+
+  const sortByCreatedAt = sortBy(prop('createdAt'));
+  const sortedData = sortByCreatedAt(data);
+
+  const getFinalData = (data) => {
+    const finalData = data.reduce((acc, current) => {
+      const year = getYear(current.createdAt);
+      if (year !== 2019) return acc;
+
+      const month = getMonth(current.createdAt);
+      if (acc[month]) {
+        acc[month] = acc[month] + 1;
+      } else {
+        acc[month] = 1;
+      }
+      return acc;
+    }, {});
+    //   console.log(finalData)
+    return Object.keys(finalData).map((key) => ({
+      month: key,
+      postCount: finalData[key],
+    }));
+  };
+
+  return getFinalData(sortedData);
+}
